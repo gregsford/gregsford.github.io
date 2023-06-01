a7c0c0ca459c2f1df497f7a9b98a118f66afbbb9
Greg Ford <88810845+gregsford@users.noreply.github.com>
2023-05-31 20:58:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
 
-// Baby Name Functions
-function babyNameSite() {
-    window.open("https://baby-ford-name.netlify.app/");
-}
-function babyNameGit() {
-    window.open("https://github.com/gregsford/baby-name");
-}
-
 // Baby Ford Functions
 function babySite() {
     window.open("https://fantastic-cupcake-f8ead0.netlify.app/");
 }
 function babyGit() {
     window.open("https://github.com/gregsford/baby-ford");
+}
+
+// Baby Name Functions
+function babyNameSite() {
+    window.open("https://baby-ford-name.netlify.app/");
+}
+function babyNameGit() {
+    window.open("https://github.com/gregsford/baby-name");
 }
 
 // Password Gen Functions
